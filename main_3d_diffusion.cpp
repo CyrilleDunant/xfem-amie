e3d1d5362da22bba06def557b2093b8ef78d4010
cyrille <cyrille.dunant@gmail.com>
2007-09-12 13:09:14 +0000

    optimize
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@1338 64fb8e13-074e-4207-b257-2d34fb7690c6

--- main_3d_diffusion.cpp
+++ main_3d_diffusion.cpp
@@ -415,7 +415,7 @@ int main(int argc, char *argv[])
 					K->setPoint(0,microstruct.getPoints()[i]->id) ;
 // 				if(microstruct.getPoints()[i]->z < 1e-9 && microstruct.getPoints()[i]->t > 1e-9)
 // 					K->setPoint(0,microstruct.getPoints()[i]->id) ;
-				if(microstruct.getPoints()[i]->z < 1e-9 && microstruct.getPoints()[i]->t > 1e-9)
+				if(microstruct.getPoints()[i]->x < 1e-9 && microstruct.getPoints()[i]->t > 1e-9)
 					K->setPoint(.2,microstruct.getPoints()[i]->id) ;
 		}
 	}
