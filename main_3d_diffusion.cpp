0d3a9ee25e0426a8f76c119968e5a0f620d672a4
cyrille <cyrille.dunant@gmail.com>
2007-09-12 09:44:52 +0000

    optimize
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@1328 64fb8e13-074e-4207-b257-2d34fb7690c6

--- main_3d_diffusion.cpp
+++ main_3d_diffusion.cpp
@@ -408,23 +408,17 @@ int main(int argc, char *argv[])
 	
 	std::cout << "adding done" << std::endl ;
 	
-	for(size_t i = 0 ; i < microstruct.getElements().size() ; i++)	
+	for(size_t i = 0 ; i < microstruct.getPoints().size() ; i++)	
 	{
 		if(i%100 == 0)
-			std::cout << "\rBC element " << i << "/" << microstruct.getElements().size() << std::flush ;
-		if(microstruct.getElements()[i]->getBehaviour()->type != VOID_BEHAVIOUR)
-		{
-			for(size_t j = 0 ; j < microstruct.getElements()[i]->getBoundingPoints().size()  ; j++)
-			{
-				if(microstruct.getElements()[i]->getBoundingPoint(j).t < 1e-9)
-					K->setPoint(0,microstruct.getElements()[i]->getBoundingPoint(j).id) ;
-				if(microstruct.getElements()[i]->getBoundingPoint(j).x < 1e-9 
-				   && microstruct.getElements()[i]->getBoundingPoint(j).t > 1e-9)
-					K->setPoint(0,microstruct.getElements()[i]->getBoundingPoint(j).id) ;
-				if(std::abs(microstruct.getElements()[i]->getBoundingPoint(j).x- 7.5) < 1e-9
-					&& microstruct.getElements()[i]->getBoundingPoint(j).t > 1e-9)
-					K->setPoint(.2,microstruct.getElements()[i]->getBoundingPoint(j).id) ;
-			}
+			std::cout << "\rBC point " << i << "/" << microstruct.getPoints().size() << std::flush ;
+
+			if(microstruct.getPoints()[i]->t < 1e-9)
+				K->setPoint(0,microstruct.getElements()[i]->id) ;
+			if(microstruct.getPoints()[i]->x < 1e-9 && microstruct.getPoints()[i]->t > 1e-9)
+				K->setPoint(0,microstruct.getPoints()[i]->id) ;
+			if(std::abs(microstruct.getPoints()[i]->x- 7.5) < 1e-9 && microstruct.getPoints()[i]->t > 1e-9)
+				K->setPoint(.2,microstruct.getPoints()[i]->id) ;
 		}
 	}
 	
