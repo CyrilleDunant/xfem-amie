f4c11d6546f843d89a170c10c9938a0f5f07cfbe
cyrille <cyrille.dunant@gmail.com>
2008-03-18 20:27:21 +0000

    update
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@1487 64fb8e13-074e-4207-b257-2d34fb7690c6

--- delaunay_3d.cpp
+++ delaunay_3d.cpp
@@ -158,6 +158,7 @@ void Star_3D::updateNeighbourhood()
 						                              treeitem[j]
 					                                     )
 					                            ) ;
+				treeitem[j]->father->addSon(treeitem[i]) ;
 				treeitem[j]->stepfather->addStepson(treeitem[i]) ;
 				treeitem[i]->stepfather = treeitem[j]->stepfather ;
 			}
