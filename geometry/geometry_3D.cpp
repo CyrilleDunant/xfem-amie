14330e80b125c4b441eff1b77896b4996421e5eb
cyrille <cyrille.dunant@gmail.com>
2008-03-20 20:42:46 +0000

    sync
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@1525 64fb8e13-074e-4207-b257-2d34fb7690c6

--- geometry/geometry_3D.cpp
+++ geometry/geometry_3D.cpp
@@ -265,14 +265,14 @@ bool Tetrahedron::inCircumSphere(const Point & p) const
 	Point f(p) ; f.z -= POINT_TOLERANCE ; f.y += POINT_TOLERANCE ; f.z -= POINT_TOLERANCE ;
 	Point g(p) ; g.z -= POINT_TOLERANCE ; g.y -= POINT_TOLERANCE ; g.z += POINT_TOLERANCE ;
 	Point h(p) ; h.z -= POINT_TOLERANCE ; h.y -= POINT_TOLERANCE ; h.z -= POINT_TOLERANCE ;
-	return  squareDist3D(circumCenter, a) < sqradius 
-		&&  squareDist3D(circumCenter, b) < sqradius
-		&&  squareDist3D(circumCenter, c) < sqradius
-		&&  squareDist3D(circumCenter, d) < sqradius
-		&&  squareDist3D(circumCenter, e) < sqradius
-		&&  squareDist3D(circumCenter, f) < sqradius
-		&&  squareDist3D(circumCenter, g) < sqradius
-		&&  squareDist3D(circumCenter, h) < sqradius;
+	return  squareDist3D(circumCenter, a) < - 4.*POINT_TOLERANCE+sqradius 
+		&&  squareDist3D(circumCenter, b) < - 4.*POINT_TOLERANCE+sqradius
+		&&  squareDist3D(circumCenter, c) < - 4.*POINT_TOLERANCE+sqradius
+		&&  squareDist3D(circumCenter, d) < - 4.*POINT_TOLERANCE+sqradius
+		&&  squareDist3D(circumCenter, e) < - 4.*POINT_TOLERANCE+sqradius
+		&&  squareDist3D(circumCenter, f) < - 4.*POINT_TOLERANCE+sqradius
+		&&  squareDist3D(circumCenter, g) < - 4.*POINT_TOLERANCE+sqradius
+		&&  squareDist3D(circumCenter, h) < - 4.*POINT_TOLERANCE+sqradius;
 // 	return  fma(x,x,fma(y,y,z*z)) < sqradius*(1. - 4.*POINT_TOLERANCE);
 }
 
@@ -286,14 +286,14 @@ bool Tetrahedron::inCircumSphere(const Point *p) const
 	Point f(*p) ; f.z -= POINT_TOLERANCE ; f.y += POINT_TOLERANCE ; f.z -= POINT_TOLERANCE ;
 	Point g(*p) ; g.z -= POINT_TOLERANCE ; g.y -= POINT_TOLERANCE ; g.z += POINT_TOLERANCE ;
 	Point h(*p) ; h.z -= POINT_TOLERANCE ; h.y -= POINT_TOLERANCE ; h.z -= POINT_TOLERANCE ;
-	return  squareDist3D(circumCenter, a) < sqradius 
-		&&  squareDist3D(circumCenter, b) < sqradius
-		&&  squareDist3D(circumCenter, c) < sqradius
-		&&  squareDist3D(circumCenter, d) < sqradius
-		&&  squareDist3D(circumCenter, e) < sqradius
-		&&  squareDist3D(circumCenter, f) < sqradius
-		&&  squareDist3D(circumCenter, g) < sqradius
-		&&  squareDist3D(circumCenter, h) < sqradius;
+	return  squareDist3D(circumCenter, a) < - 4.*POINT_TOLERANCE+sqradius 
+		&&  squareDist3D(circumCenter, b) < - 4.*POINT_TOLERANCE+sqradius
+		&&  squareDist3D(circumCenter, c) < - 4.*POINT_TOLERANCE+sqradius
+		&&  squareDist3D(circumCenter, d) < - 4.*POINT_TOLERANCE+sqradius
+		&&  squareDist3D(circumCenter, e) < - 4.*POINT_TOLERANCE+sqradius
+		&&  squareDist3D(circumCenter, f) < - 4.*POINT_TOLERANCE+sqradius
+		&&  squareDist3D(circumCenter, g) < - 4.*POINT_TOLERANCE+sqradius
+		&&  squareDist3D(circumCenter, h) < - 4.*POINT_TOLERANCE+sqradius;
 // 	return   fma(x,x,fma(y,y,z*z)) < sqradius*(1. - 4.*POINT_TOLERANCE);
 }
 
