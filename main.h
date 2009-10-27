fb31188f5d8f33d40d9923f12dc627021933e6d9
cyrille <cyrille.dunant@gmail.com>
2009-10-27 13:43:47 +0000

    updata
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@1638 64fb8e13-074e-4207-b257-2d34fb7690c6

--- main.h
+++
@@ -1,9 +0,0 @@
-
-// Author: Cyrille Dunant <cyrille.dunant@epfl.ch>, (C) 2005-2007
-//
-// Copyright: See COPYING file that comes with this distribution
-//
- 
-#include "matrixops.h"
-#include <iostream>
- 
