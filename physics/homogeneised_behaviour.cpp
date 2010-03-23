717926e13dc2b3d2dc53845821324a0804d7d3b1
cyrille <cyrille.dunant@gmail.com>
2010-03-23 13:47:21 +0000

    add groundwork for homogenised behaviours
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@1742 64fb8e13-074e-4207-b257-2d34fb7690c6

---
+++ physics/homogeneised_behaviour.cpp
@@ -0,0 +1,201 @@
+//
+// C++ Implementation: stiffness
+//
+// Description: 
+//
+//
+// Author: Cyrille Dunant <cyrille.dunant@epfl.ch>, (C) 2007
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#include "homogeneised_behaviour.h"
+
+using namespace Mu ;
+
+HomogeneisedBehaviour::HomogeneisedBehaviour(Mesh<DelaunayTriangle, DelaunayTreeItem> * mesh2d, DelaunayTriangle * self) : LinearForm(Matrix(), false, false, 1) , mesh2d(mesh2d), self2d(self), mesh3d(NULL), self3d(NULL) 
+{
+	std::vector<DelaunayTriangle *> source = mesh2d->getConflictingElements(self->getPrimitive()) ;
+	//simple averaging
+	double totalArea = 0 ;
+	param = Matrix(source[0]->getBehaviour()->param.numRows(), source[0]->getBehaviour()->param.numCols()) ;
+	for(size_t i = 0 ; i < source.size() ; i++)
+	{
+		time_d = time_d || source[i]->getBehaviour()->timeDependent() ;
+		space_d = space_d || source[i]->getBehaviour()->spaceDependent() ;
+		
+		double area = source[i]->area() ;
+		totalArea += area ;
+		
+		GaussPointArray gp = source[i]->getGaussPoints() ;
+		for(size_t j = 0 ; j < gp.gaussPoints.size() ; j++)
+		{
+			param += source[i]->getBehaviour()->getTensor(gp.gaussPoints[j].first) * gp.gaussPoints[j].second / area ;
+		}
+	}
+	
+	param /= totalArea ;
+	v.push_back(XI);
+	v.push_back(ETA);
+} ;
+
+HomogeneisedBehaviour::HomogeneisedBehaviour(Mesh<DelaunayTetrahedron, DelaunayTreeItem3D> * mesh3d, DelaunayTetrahedron * self) : LinearForm(Matrix(), false, false, 1), mesh2d(NULL), self2d(NULL), mesh3d(mesh3d), self3d(self) 
+{
+	std::vector<DelaunayTetrahedron *> source = mesh3d->getConflictingElements(self->getPrimitive()) ;
+	//simple averaging
+	double totalVolume = 0 ;
+	param = Matrix(source[0]->getBehaviour()->param.numRows(), source[0]->getBehaviour()->param.numCols()) ;
+	for(size_t i = 0 ; i < source.size() ; i++)
+	{
+		time_d = time_d || source[i]->getBehaviour()->timeDependent() ;
+		space_d = space_d || source[i]->getBehaviour()->spaceDependent() ;
+		
+		double volume = source[i]->area() ;
+		totalVolume += volume ;
+		
+		GaussPointArray gp = source[i]->getGaussPoints() ;
+		for(size_t j = 0 ; j < gp.gaussPoints.size() ; j++)
+		{
+			param += source[i]->getBehaviour()->getTensor(gp.gaussPoints[j].first) * gp.gaussPoints[j].second / volume ;
+		}
+	}
+	
+	param /= totalVolume ;
+	
+	v.push_back(XI);
+	v.push_back(ETA);
+	v.push_back(ZETA);
+} ;
+
+HomogeneisedBehaviour::~HomogeneisedBehaviour() { } ;
+
+Matrix HomogeneisedBehaviour::apply(const Function & p_i, const Function & p_j, const IntegrableEntity *e) const
+{
+	return VirtualMachine().ieval(Gradient(p_i) * param * Gradient(p_j, true), e,v) ;
+}
+
+void HomogeneisedBehaviour::apply(const Function & p_i, const Function & p_j, const GaussPointArray &gp, const std::valarray<Matrix> &Jinv, Matrix & ret, VirtualMachine * vm) const
+{
+// std::cout << "a--" << std::endl ;
+// Jinv[0].print() ;
+// std::cout << "--b" << std::endl ;
+	vm->ieval(Gradient(p_i) * param * Gradient(p_j, true), gp, Jinv,v, ret) ;
+// 	ret.print() ;
+}
+
+void HomogeneisedBehaviour::step(double timestep, ElementState & currentState)
+{
+	if(currentState.getParent()->spaceDimensions() == 2)
+	{
+		std::vector<DelaunayTriangle *> source = mesh2d->getConflictingElements(self2d->getPrimitive()) ;
+		//simple averaging
+		double totalArea = 0 ;
+		param = Matrix(source[0]->getBehaviour()->param.numRows(), source[0]->getBehaviour()->param.numCols()) ;
+		for(size_t i = 0 ; i < source.size() ; i++)
+		{
+			time_d = time_d || source[i]->getBehaviour()->timeDependent() ;
+			space_d = space_d || source[i]->getBehaviour()->spaceDependent() ;
+			
+			double area = source[i]->area() ;
+			totalArea += area ;
+			
+			GaussPointArray gp = source[i]->getGaussPoints() ;
+			for(size_t j = 0 ; j < gp.gaussPoints.size() ; j++)
+			{
+				param += source[i]->getBehaviour()->getTensor(gp.gaussPoints[j].first) * gp.gaussPoints[j].second / area ;
+			}
+		}
+		
+		param /= totalArea ;
+	}
+	else
+	{
+		std::vector<DelaunayTetrahedron *> source = mesh3d->getConflictingElements(self3d->getPrimitive()) ;
+		//simple averaging
+		double totalVolume = 0 ;
+		param = Matrix(source[0]->getBehaviour()->param.numRows(), source[0]->getBehaviour()->param.numCols()) ;
+		for(size_t i = 0 ; i < source.size() ; i++)
+		{
+			time_d = time_d || source[i]->getBehaviour()->timeDependent() ;
+			space_d = space_d || source[i]->getBehaviour()->spaceDependent() ;
+			
+			double volume = source[i]->area() ;
+			totalVolume += volume ;
+			
+			GaussPointArray gp = source[i]->getGaussPoints() ;
+			for(size_t j = 0 ; j < gp.gaussPoints.size() ; j++)
+			{
+				param += source[i]->getBehaviour()->getTensor(gp.gaussPoints[j].first) * gp.gaussPoints[j].second / volume ;
+			}
+		}
+		
+		param /= totalVolume ;
+	}
+}
+
+void HomogeneisedBehaviour::stepBack()
+{
+	if(self2d)
+	{
+		std::vector<DelaunayTriangle *> source = mesh2d->getConflictingElements(self2d->getPrimitive()) ;
+		//simple averaging
+		double totalArea = 0 ;
+		param = Matrix(source[0]->getBehaviour()->param.numRows(), source[0]->getBehaviour()->param.numCols()) ;
+		for(size_t i = 0 ; i < source.size() ; i++)
+		{
+			time_d = time_d || source[i]->getBehaviour()->timeDependent() ;
+			space_d = space_d || source[i]->getBehaviour()->spaceDependent() ;
+			
+			double area = source[i]->area() ;
+			totalArea += area ;
+			
+			GaussPointArray gp = source[i]->getGaussPoints() ;
+			for(size_t j = 0 ; j < gp.gaussPoints.size() ; j++)
+			{
+				param += source[i]->getBehaviour()->getTensor(gp.gaussPoints[j].first) * gp.gaussPoints[j].second / area ;
+			}
+		}
+		
+		param /= totalArea ;
+	}
+	else
+	{
+		std::vector<DelaunayTetrahedron *> source = mesh3d->getConflictingElements(self3d->getPrimitive()) ;
+		//simple averaging
+		double totalVolume = 0 ;
+		param = Matrix(source[0]->getBehaviour()->param.numRows(), source[0]->getBehaviour()->param.numCols()) ;
+		for(size_t i = 0 ; i < source.size() ; i++)
+		{
+			time_d = time_d || source[i]->getBehaviour()->timeDependent() ;
+			space_d = space_d || source[i]->getBehaviour()->spaceDependent() ;
+			
+			double volume = source[i]->area() ;
+			totalVolume += volume ;
+			
+			GaussPointArray gp = source[i]->getGaussPoints() ;
+			for(size_t j = 0 ; j < gp.gaussPoints.size() ; j++)
+			{
+				param += source[i]->getBehaviour()->getTensor(gp.gaussPoints[j].first) * gp.gaussPoints[j].second / volume ;
+			}
+		}
+		
+		param /= totalVolume ;
+	}
+}
+
+
+bool HomogeneisedBehaviour::fractured() const
+{
+	return false ;
+}
+
+Form * HomogeneisedBehaviour::getCopy() const 
+{
+	return new HomogeneisedBehaviour(*this) ;
+}
+
+void HomogeneisedBehaviour::getForces(const ElementState & s, const Function & p_i, const GaussPointArray &gp, const std::valarray<Matrix> &Jinv, Vector & f) const 
+{
+}
+
---
+++ physics/homogeneised_behaviour.h
@@ -0,0 +1,85 @@
+//
+// C++ Interface: stiffness
+//
+// Description: 
+//
+//
+// Author: Cyrille Dunant <cyrille.dunant@epfl.ch>, (C) 2007
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#ifndef __HOMOGENEISED_H_
+#define __HOMOGENEISED_H_
+
+#include "physics_base.h"
+#include "../mesher/mesh.h"
+#include "../mesher/delaunay.h"
+#include "../mesher/delaunay_3d.h"
+
+namespace Mu
+{
+
+	/** \brief A linear Elastic Law
+	* The field param is the Cauchy-Green Strain Tensor
+	*/
+	struct HomogeneisedBehaviour : public LinearForm
+	{
+		Mesh<DelaunayTriangle, DelaunayTreeItem> * mesh2d ;
+		DelaunayTriangle * self2d ;
+		Mesh<DelaunayTetrahedron, DelaunayTreeItem3D> * mesh3d ;
+		DelaunayTetrahedron * self3d ;
+		std::vector<Variable> v ;
+		/** \brief Constructor
+		* 
+		* @param mesh2d The 2D mesh for 2D homogeneisation
+		* @param self The element in which the homogeneisation will occur
+		*/
+		HomogeneisedBehaviour(Mesh<DelaunayTriangle, DelaunayTreeItem> * mesh2d, DelaunayTriangle * self) ;
+		/** \brief Constructor
+		* 
+		* @param mesh3d The 3D mesh for 3D homogeneisation
+		* @param self The element in which the homogeneisation will occur
+		*/
+		HomogeneisedBehaviour(Mesh<DelaunayTetrahedron, DelaunayTreeItem3D> * mesh3d, DelaunayTetrahedron * self) ;
+		
+		virtual ~HomogeneisedBehaviour() ;
+		
+		/** \brief Apply the law.
+		* 
+		* @param p_i first basis polynomial.
+		* @param p_j second basis polynomial.
+		* @return symbolic matrix resulting of \f$ \nabla H^T K \nabla H \f$.
+		*/
+		virtual Matrix apply(const Function & p_i, const Function & p_j, const IntegrableEntity *e) const; 
+		
+		/** \brief Apply the law.
+		 *
+		 * The matrix is computed as: \f$ \nabla^T h_i K \nabla h_j \f$
+		 * @param p_i first basis polynomial.
+		 * @param p_j second basis polynomial.
+		 * @param gp Gauss Points used for the quadrature
+		 * @param Jinv Inverse Jacobian Matrices corresponding to the gauss points
+		 * @param ret Matrix to store the result
+		 * @param vm virtualMachine to use to compute the result
+		 */
+		virtual void apply(const Function & p_i, const Function & p_j, const GaussPointArray &gp, const std::valarray<Matrix> &Jinv, Matrix & ret, VirtualMachine * vm) const ;
+		
+		/** \brief Return false.*/
+		virtual bool fractured() const ;
+		
+		/** \brief Return a copy of the behaviour*/
+		virtual Form * getCopy() const ;
+		
+		/** \brief Return a 0-length Vector*/
+		virtual void getForces(const ElementState & s, const Function & p_i, const GaussPointArray &gp, const std::valarray<Matrix> &Jinv, Vector &v) const ;
+		
+		virtual void step(double timestep, ElementState & currentState) ;
+		virtual void stepBack() ;
+		
+	} ;
+
+} ;
+
+#endif //__HOMOGENEISED_H_
