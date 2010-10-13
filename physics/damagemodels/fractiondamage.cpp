1282fe41eba074f88eb2f5df90b8659f859d4834
cyrille <cyrille.dunant@gmail.com>
2010-10-13 21:50:07 +0000

    new utility damage models
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@1931 64fb8e13-074e-4207-b257-2d34fb7690c6

---
+++ physics/damagemodels/fractiondamage.cpp
@@ -0,0 +1,152 @@
+//
+// C++ Implementation: lineardamage
+//
+// Description: 
+//
+//
+// Author: Cyrille Dunant <cyrille.dunant@epfl.ch>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "fractiondamage.h"
+#include "../fracturecriteria/fracturecriterion.h"
+
+namespace Mu {
+
+FractionLinearDamage::FractionLinearDamage(int numDof, double characteristicRadius, Matrix remnant, double phi) : DamageModel(characteristicRadius), remnant(remnant), phi(phi)
+{
+	state.resize(2, 0.) ;
+	isNull = false ;
+	state = 0 ;
+	tensionDamage = 0 ;
+	compressionDamage = 0 ;
+	inCompression = false ;
+	inTension = false ;
+}
+
+const Vector & FractionLinearDamage::damageState() const
+{
+	return state ;
+}
+
+
+Vector & FractionLinearDamage::damageState() 
+{
+	return state ;
+}
+void FractionLinearDamage::step(ElementState & s)
+{
+	previousstate.resize(state.size());
+	previousstate = state ;
+	inCompression = false ;
+	inTension = false ;
+	if(fraction < 0)
+	{
+		double volume ;
+		if(s.getParent()->spaceDimensions() == SPACE_TWO_DIMENSIONAL)
+			volume = sqrt(s.getParent()->area()) ;
+		else
+			volume = pow(s.getParent()->volume(), 2./3.) ;
+		
+		double charVolume ;
+		if(s.getParent()->spaceDimensions() == SPACE_TWO_DIMENSIONAL)
+			charVolume = sqrt(M_PI*characteristicRadius*characteristicRadius) ;
+		else
+			charVolume = pow(4./3.*M_PI*characteristicRadius*characteristicRadius*characteristicRadius, 2./3.) ;
+		fraction = volume/charVolume ;
+		if(fraction > 1)
+			std::cout << "elements too large for damage characteristic radius!" << std::endl ;
+		fraction = std::min(fraction, 1.) ;
+	}
+	
+
+	if(s.getParent()->getBehaviour()->getFractureCriterion()->metInCompression)
+	{
+		inCompression = true ;
+		
+		compressionDamage += damageDensityIncrement*fraction ; 
+		compressionDamage = std::min(thresholdDamageDensity/fraction+POINT_TOLERANCE, compressionDamage) ;
+		compressionDamage = std::min(.99999, compressionDamage) ;
+		compressionDamage = std::max(0., compressionDamage) ;
+	}
+	
+	if(s.getParent()->getBehaviour()->getFractureCriterion()->metInTension)
+	{
+		inTension = true ;
+
+		tensionDamage += damageDensityIncrement*fraction ; 
+		tensionDamage = std::min(secondaryThresholdDamageDensity/fraction+POINT_TOLERANCE, tensionDamage) ;
+		tensionDamage = std::min(.99999, tensionDamage) ;
+		tensionDamage = std::max(0., tensionDamage) ;
+	}
+	state[0] = compressionDamage ;
+	state[1] = tensionDamage ;
+// 	std::cout << state.sum() << std::flush ;
+}
+
+void FractionLinearDamage::artificialDamageStep(double d)
+{
+	for(size_t i = 0 ; i < state.size() -1 ; i++)
+		state[i] = std::min(state[i]+d,0.9999) ;
+}
+
+Matrix FractionLinearDamage::apply(const Matrix & m) const
+{
+	Matrix ret(m*(1.-phi)+remnant*phi) ;
+	
+	if(fractured())
+		return remnant*phi;
+
+	if(inTension)
+	{
+		return m*(1.-tensionDamage)*(1.-phi)+remnant*phi ;
+	}
+	if(inCompression)
+	{
+		return m*(1.-compressionDamage)*(1.-phi)+remnant*phi ;
+	}
+	return ret*(1.-std::max(tensionDamage, compressionDamage))*(1.-phi)+remnant*phi ;
+}
+
+Matrix FractionLinearDamage::applyPrevious(const Matrix & m) const
+{
+	Matrix ret(m) ;
+	
+	if(fractured())
+		return ret*0;
+	//this is a silly way of distinguishing between 2D and 3D
+	for(size_t i = 0 ; i < (m.numRows()+1)/2 ;i++)
+	{
+		for(size_t j = 0 ; j < m.numCols() ;j++)
+		{
+			ret[i][j] *= 1.-(previousstate[0]) ;
+		}
+	}
+
+	for(size_t i = (m.numRows()+1)/2 ; i < m.numRows() ;i++)
+	{
+		for(size_t j = 0 ; j < m.numCols() ;j++)
+		{
+			ret[i][j] *= 1.-(previousstate[i]) ;
+		}
+	}
+
+	return ret ;
+}
+
+bool FractionLinearDamage::fractured() const
+{
+	if (fraction < 0)
+		return false ;
+	
+// 	std::cout << std::max(tensionDamage, compressionDamage) <<  " " << thresholdDamageDensity/**fraction*/ << std::endl ;
+	return tensionDamage >= secondaryThresholdDamageDensity/fraction || compressionDamage >= thresholdDamageDensity/fraction  ;
+}
+
+FractionLinearDamage::~FractionLinearDamage()
+{
+}
+
+
+}
---
+++ physics/fraction_stiffness_and_fracture.h
@@ -0,0 +1,128 @@
+//
+// C++ Interface: stiffness_and_fracture
+//
+// Description: 
+//
+//
+// Author: Cyrille Dunant <cyrille.dunant@epfl.ch>, (C) 2007
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#ifndef _FRACT_STIFFNESS_AND_FRACTURE_H
+#define _FRACT_STIFFNESS_AND_FRACTURE_H
+
+#include "physics_base.h"
+#include "fracturecriteria/fracturecriterion.h"
+#include "damagemodels/fractiondamage.h"
+#include "damagemodels/anisotropicdamage.h"
+#include "damagemodels/isotropiclineardamage.h"
+#include "homogenization/homogenization_base.h"
+
+namespace Mu
+{
+
+	/** \brief A linear Elastic Law with a pluggable fracture criterion
+	*
+	* The field param is the Cauchy-Green Strain Tensor
+	*/
+	struct FractionStiffnessAndFracture : public LinearForm
+	{
+// 		AnisotropicLinearDamage * dfunc ;
+		FractionLinearDamage * dfunc ;
+// 		IsotropicLinearDamage dfunc ;
+		double eps ;
+		Vector previousPreviousDamage ;
+		Vector intermediateDamage ;
+		Vector previousDamage ;
+		int count ;
+		FractureCriterion * criterion ;
+		bool frac ;
+		bool change ; 
+		bool previouschange ; 
+		double sigmaRupt ;
+		double init ;
+		Vector damage ;
+		std::vector<Variable> v ;
+
+		/** \brief Constructor
+		* 
+		* @param rig Complete expression of the Cauchy-Green Strain Tensor
+		* @param c  FractureCriterion to use. The behaviour is responsible for deleting the criterion upon cleanup.
+		*/
+		FractionStiffnessAndFracture(const Matrix & rig, const Matrix & rig0, double phi, FractureCriterion * c, double eps = 0.002)  ;
+		void setNeighbourhoodRadius(double d ) ;
+
+		virtual ~FractionStiffnessAndFracture();
+		
+		/** Apply the law.
+		* @param p_i first basis polynomial.
+		* @param p_j second basis polynomial.
+		* @param gp integration points
+		* @param Jinv inverse Jacobian matrix at the integration points
+		* @param ret, matrix to store the result
+		* @param vm VirtualMachine to use to perform the computation
+		* @return matrix resulting of \f$ \nabla H^T K \nabla H \f$.
+		*/
+		virtual void apply(const Function & p_i, const Function & p_j, const GaussPointArray &gp, const std::valarray<Matrix> &Jinv, Matrix & ret, VirtualMachine * vm) const ;
+		
+		/** \brief Check for fracture
+		*
+		* @param timestep elapsed time
+		* @param currentState state of the element
+		* 
+		* if the yield criterion is true, se fractured state to true
+		*/
+		virtual void step(double timestep, ElementState & currentState) ;
+		
+		/** Artificial damage step */
+		virtual void artificialDamageStep(double d) ;
+
+		/** \brief returns the previous damage */
+		virtual Vector getPreviousDamage() {return previousDamage ; } ;
+
+		/** \brief returns the previous previous damage */
+		virtual Vector getPreviousPreviousDamage() {return previousPreviousDamage ; } ;
+
+		/** \brief Impose previous and previous previous damage */
+		virtual void artificialPreviousDamage(Vector previous, Vector previousprevious) ;
+
+		/** \brief Check for fracture state
+		*
+		* @return true if the element is fractured
+		*/
+		virtual bool fractured() const ;
+		
+		
+		/** \brief get Copy of the behaviour
+		*
+		* The copy also copies the damage state.
+		* @return pointer to the copy. Caller is responsible for cleaning memory
+		*/
+		virtual Form * getCopy() const ;
+
+		/** \brief return true if the damage state has been modfied*/
+		virtual bool changed() const ;
+		
+		/** \brief return to the previous damage state*/
+		virtual void stepBack() ;
+
+		/** \brief Return the (damaged) Stifness tensor*/
+		virtual Matrix getTensor(const Point & p) const ;
+
+		/** \brief Acessor, return the stifness criterion in use*/
+		virtual FractureCriterion * getFractureCriterion() const ;
+
+		virtual DamageModel * getDamageModel() const ;
+		
+		virtual Material toMaterial() ;
+		
+	} ;
+	
+
+
+} ;
+
+
+#endif 
