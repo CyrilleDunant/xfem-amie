8c4014d3e15f4a42633a6984fdd69d4e9184686b
alain <alain.b.giorla@gmail.com>
2012-10-22 08:31:58 +0000

    files added
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@2395 64fb8e13-074e-4207-b257-2d34fb7690c6

---
+++ physics/stiffness_with_imposed_stress.h
@@ -0,0 +1,52 @@
+//
+// C++ Interface: stiffness_with_imposed_stress
+//
+// Description: 
+//
+//
+// Author: Cyrille Dunant <cyrille.dunant@gmail.com>, (C) 2007-2011
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#ifndef __STIFFNESS_WITH_STRESS
+#define __STIFFNESS_WITH_STRESS
+
+#include "physics_base.h"
+#include "homogenization/homogenization_base.h" 
+
+namespace Mu
+{
+	struct StiffnessWithImposedStress : public LinearForm
+	{
+		std::vector<Variable> v ;
+		Vector imposed ;
+
+		StiffnessWithImposedStress(const Matrix & rig) ;
+		StiffnessWithImposedStress(const Matrix & rig, Vector imposedStress) ;
+		StiffnessWithImposedStress(double E, double nu, double beta, SpaceDimensionality dim) ;
+		virtual ~StiffnessWithImposedStress() ;
+		
+		virtual void apply(const Function & p_i, const Function & p_j, const GaussPointArray &gp, const std::valarray<Matrix> &Jinv, Matrix & ret, VirtualMachine * vm) const ;
+
+		virtual bool fractured() const ;
+		virtual Form * getCopy() const ;
+		virtual bool hasInducedForces() const { return true ; }
+		
+		virtual Vector getImposedStress(const Point & p, IntegrableEntity * e, int g = -1) const ;
+		virtual Vector getImposedStrain(const Point & p, IntegrableEntity * e, int g = -1) const ;
+
+		std::vector<BoundaryCondition * > getBoundaryConditions(const ElementState & s,  size_t id, const Function & p_i, const GaussPointArray &gp, const std::valarray<Matrix> &Jinv) const ;
+		
+		virtual void step(double timestep, ElementState & currentState, double maxScore) ;
+
+		virtual void resetImposedStress() ;
+		virtual void setImposedStress(Vector imp) ;
+		virtual void addImposedStress(Vector imp) ;
+		
+	} ;
+
+} ;
+
+#endif
