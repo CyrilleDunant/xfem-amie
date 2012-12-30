b4a8d88d895543f375c83fe202570629e37608bd
alain <alain.b.giorla@gmail.com>
2012-12-30 16:06:51 +0000

    test again
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@2440 64fb8e13-074e-4207-b257-2d34fb7690c6

--- examples/main_rag_visco.cpp
+++ examples/main_rag_visco.cpp
@@ -261,7 +261,7 @@ int main(int argc, char *argv[])
 
 		damage = reaction(zones) + getDamagedArea(all)-getDamagedArea(paste) ;
 
-		out << time << "\t" << reaction(zones) << "\t" << aggregate_area << "\t" << damage << "\t" << x[0] << "\t" << x[1] << "\t" << y[0] << "\t" << y[1] << "\t" << z[0] << "\t" << z[1] <<  getDamagedArea(paste) << "\t" <<  getDamagedArea(all) << "\t" << getCrackedArea(paste) << "\t" << getCrackedArea(all) << std::endl ;
+		out << time << "\t" << reaction(zones) << "\t" << aggregate_area << "\t" << damage << "\t" << x[0] << "\t" << x[1] << "\t" << y[0] << "\t" << y[1] << "\t" << z[0] << "\t" << z[1] <<  "\t" << getDamagedArea(paste) << "\t" <<  getDamagedArea(all) << "\t" << getCrackedArea(paste) << "\t" << getCrackedArea(all) << std::endl ;
 	}
 		
 	return 0 ;
