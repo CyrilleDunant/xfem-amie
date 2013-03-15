31957dbba7e0567ed1da2471c67c0ef5cffa4618
giorla <alain.b.giorla@gmail.com>
2013-03-15 16:10:02 +0000

    tensor files added
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@2467 64fb8e13-074e-4207-b257-2d34fb7690c6

---
+++ utilities/tensor.cpp
@@ -0,0 +1,111 @@
+#include "tensor.h"
+
+using namespace Mu ;
+
+Tensor::Tensor(size_t o, size_t d) : order(o), dim(d) 
+{
+	size_t s = 1 ;
+	for(size_t i = 0 ; i < o ; i++)
+		s *= dim ;
+	components.resize(s) ;
+	components = 0. ;
+}
+
+Tensor::Tensor(const Point & p, std::vector<Variable> v) : order(1), dim(v.size())
+{
+	size_t s = 1 ;
+	for(size_t i = 0 ; i < order ; i++)
+		s *= dim ;
+	components.resize(s) ;
+	components = 0. ;
+	for(int i = 0 ; i < v.size() ; i++)
+	{
+		double d = 0 ;
+		switch(v[i])
+		{
+		  case XI:
+		    d = p.x ; 
+		    break ;
+		  case ETA:
+		    d = p.y ; 
+		    break ;
+		  case ZETA:
+		    d = p.z ; 
+		    break ;
+		  case TIME_VARIABLE:
+		    d = p.t ; 
+		    break ;
+		}
+		components[i] = d ;
+	}
+}
+
+Tensor::Tensor(const Matrix & m, bool transpose) : order(2), dim(m.numRows())
+{
+	components.resize(dim*dim) ;
+	for(int i = 0 ; i < dim ; i++)
+	{
+		for(int j = 0 ; j < dim ; j++)
+		{
+			if(transpose)
+				(*this)(j,i) = m[i][j] ;
+			else
+				(*this)(i,j) = m[i][j] ;
+		}
+	}
+}
+
+size_t Tensor::index( std::vector<int> indexes ) const
+{
+	if(indexes.size() == 1)
+		return indexes[0] ;
+	size_t i = 0 ;
+	size_t s = 1 ;
+	for(int k = order ; k > 0 ; k--)
+	{
+		i += (indexes[k-1]) * s ;
+		s *= dim ;
+	}
+	return i ;
+}
+
+void Tensor::print() const
+{
+	for(size_t i = 0 ; i < components.size() ; i++)
+	{
+		std::cout << components[i] << "\t" ;
+	}
+	std::cout << std::endl ;
+}
+
+void Tensor::threshold(double thr) 
+{
+	for(size_t i = 0 ; i < components.size() ; i++)
+	{
+		if(std::abs(components[i]) < thr)
+			  components[i] = 0 ;
+	}
+	
+}
+
+Matrix Tensor::toMatrix(int d1, int d2) const
+{
+	Matrix ret ;
+	if(d1+d2 != order)
+		return ret ;
+
+	Tensor dummy1( d1, dim ) ;
+	Tensor dummy2( d2, dim ) ;
+	
+	ret = Matrix(dummy1.size(), dummy2.size()) ;
+	for(int i = 0 ; i < dummy1.size() ; i++)
+	{
+		for(int j = 0 ; j < dummy2.size() ; j++)
+		{
+			ret[i][j] = components[ i*dummy2.size() + j ] ;
+		}
+	}
+	
+	return ret ;
+	
+}
---
+++ utilities/tensor.h
@@ -0,0 +1,56 @@
+#ifndef TENSOR_H
+#define TENSOR_H
+
+#include "matrixops.h"
+#include "../geometry/geometry_base.h" 
+#include "../polynomial/variable.h" 
+
+namespace Mu
+{
+
+class Tensor
+{
+protected:
+	size_t order ;
+	size_t dim ;
+	Vector components ;
+	
+public:
+	Tensor(size_t o, size_t d) ;
+	Tensor(const Point & p, std::vector<Variable> v) ;
+	Tensor(const Matrix & m, bool transpose = false) ;
+	
+	size_t getOrder() const { return order ; }
+	size_t getDimensions() const { return dim ; }
+
+	size_t index( std::vector<int> indexes ) const ;
+	size_t size() const { return components.size() ; }
+	
+	double & getComponent( std::vector<int> indexes ) { return components[ index(indexes) ] ;  }
+	double getComponent( std::vector<int> indexes ) const { return components[ index(indexes) ] ; }
+	
+	double & operator() (int i) { return components[ index(std::vector<int>{i}) ] ; }
+	double & operator() (int i, int j) { return components[ index(std::vector<int>{i,j}) ] ; }
+	double & operator() (int i, int j, int k) { return components[ index(std::vector<int>{i,j,k}) ] ; }
+	double & operator() (int i, int j, int k, int l) { return components[ index(std::vector<int>{i,j,k,l}) ] ; }
+	double & operator() (int i, int j, int k, int l, int m){ return components[ index(std::vector<int>{i,j,k,l,m}) ] ; }
+	double & operator() (int i, int j, int k, int l, int m, int n){ return components[ index(std::vector<int>{i,j,k,l,m,n}) ] ; }
+
+	double operator() (int i) const { return components[ index(std::vector<int>{i}) ] ; }
+	double operator() (int i, int j) const { return components[ index(std::vector<int>{i,j}) ] ; }
+	double operator() (int i, int j, int k) const { return components[ index(std::vector<int>{i,j,k}) ] ; }
+	double operator() (int i, int j, int k, int l) const { return components[ index(std::vector<int>{i,j,k,l}) ] ; }
+	double operator() (int i, int j, int k, int l, int m) const { return components[ index(std::vector<int>{i,j,k,l,m}) ] ; }
+	double operator() (int i, int j, int k, int l, int m, int n) const { return components[ index(std::vector<int>{i,j,k,l,m,n}) ] ; }
+	
+	Matrix toMatrix(int d1, int d2) const ;
+	
+	void print() const ;
+	
+	void threshold(double thr) ;
+		
+  
+} ;
+
+} ;
+#endif // TENSOR_H
