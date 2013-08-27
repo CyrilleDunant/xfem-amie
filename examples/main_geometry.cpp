9fafc5ec75ad6b6a7d413ea2d72dda4f4cb70c65
cubaramo <a.cubaramos@gmail.com>
2013-08-27 10:08:55 +0000

    boundary conditions added
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@2627 64fb8e13-074e-4207-b257-2d34fb7690c6

--- examples/main_geometry.cpp
+++ examples/main_geometry.cpp
@@ -70,8 +70,8 @@ int main(int argc, char *argv[])
   // creates 3D inclusions with rmax = 0.002, covering a volume of 0.000252, using BOLOME type B particle sizs distribution
   // psd ends when smallest radius reaches 0.00025 or when 1000 inclusions have been generated
   // covering volume 0.000042
-  std::vector<Inclusion3D *> incs = ParticleSizeDistribution::get3DInclusions(0.002, 0.000042, BOLOME_B, PSDEndCriteria(0.00025, -1, 3200)) ;
-  
+  //std::vector<Inclusion3D *> incs = ParticleSizeDistribution::get3DInclusions(0.002, 0.000042, BOLOME_B, PSDEndCriteria(0.00025, -1, 3200)) ;
+  std::vector<Inclusion3D *> incs = ParticleSizeDistribution::get3DInclusions(0.002, 0.000023104, BOLOME_B, PSDEndCriteria(0.00025, -1, 3200)) ;
   // attributes mechanical behaviour to the box and the aggregates
   box.setBehaviour( new ElasticOnlyPasteBehaviour( 12e9, 0.3, SPACE_THREE_DIMENSIONAL ) ) ;
   for(size_t i = 0 ; i < incs.size() ; i++)
@@ -95,7 +95,7 @@ int main(int argc, char *argv[])
     }
 	
   // sampling criteria
-  F.setSamplingNumber( 512*16 ) ;
+  F.setSamplingNumber(14000); //512*16 ) ;
 
   std::ofstream inclusions;
   inclusions.open("inclusions.csv");
