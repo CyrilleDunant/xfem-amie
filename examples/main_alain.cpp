be539da18818aa06994ad730e4e4252eb0495531
giorla <alain.b.giorla@gmail.com>
2014-05-16 13:54:51 +0000

    test case
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@2740 64fb8e13-074e-4207-b257-2d34fb7690c6

--- examples/main_alain.cpp
+++ examples/main_alain.cpp
@@ -11,15 +11,20 @@
 #include "../physics/kelvinvoight.h"
 #include "../physics/maxwell.h"
 #include "../physics/stiffness.h"
+#include "../physics/stiffness_and_fracture.h"
 #include "../physics/parallel_behaviour.h"
-#include "../physics/viscoelasticity.h"
-#include "../physics/viscoelasticity_and_fracture.h"
+#include "../physics/fracturecriteria/mohrcoulomb.h"
 #include "../physics/fracturecriteria/maxstrain.h"
+#include "../physics/fracturecriteria/creeprupture.h"
 #include "../physics/fracturecriteria/ruptureenergy.h"
 #include "../physics/weibull_distributed_stiffness.h"
-#include "../features/pore.h"
 #include "../physics/damagemodels/spacetimefiberbasedisotropiclineardamage.h"
+#include "../physics/damagemodels/fiberbasedisotropiclineardamage.h"
+#include "../features/pore.h"
 #include "../utilities/writer/triangle_writer.h"
+#include "../physics/materials/gel_behaviour.h"
+#include "../physics/viscoelasticity.h"
+#include "../physics/viscoelasticity_and_fracture.h"
 #include "../physics/materials/paste_behaviour.h"
 #include "../physics/materials/aggregate_behaviour.h"
 #include "../physics/homogenization/homogenization_base.h"
@@ -38,135 +43,231 @@
 
 #include <fstream>
 
+#include <omp.h>
 #include <cmath>
 #include <typeinfo>
 #include <limits>
 #include <sys/time.h>
 #define DEBUG
+
+
 using namespace Mu ;
 
-Sample box(nullptr, 0.08, 0.08,0.,0.) ;
+// see Cecot 2001, p98, for sample dimensions
 
+double length = 0.2 ; //5.5 ;
+double width = 0.045 ;
+double depth = 0.03 ;//2.5 ;
+double nnotch = 0.085 ;
+double nwidth = 0.0025 ;
+Sample box(nullptr, length, length,0.,length*0.5) ;
+Sample top(nullptr, width, depth, 0.0, length - depth*0.5) ;
+Sample notch(nullptr,nwidth, nnotch, 0.0, length - depth - nnotch*0.5) ;
 
 int main(int argc, char *argv[])
 {
-	double timestep = 1. ;//atof(argv[1]) ;
+//	 omp_set_schedule(omp_sched_static, 60) ;
+// 	omp_set_num_threads(4) ;
+
+//	srandom((int) atof(argv[3])) ;
 	FeatureTree F(&box) ;
-	F.setSamplingNumber(50) ;
-
-	ViscoElasticOnlyPasteBehaviour dpaste ;
-	SpaceTimeFiberBasedIsotropicLinearDamage * dampaste = new SpaceTimeFiberBasedIsotropicLinearDamage( 0.05, atof(argv[1]) ) ;
-	dampaste->setLogitViscousDamageLaw(0.025, 0.3, 2.5) ;
-	ViscoelasticityAndFracture paste( PURE_ELASTICITY, dpaste.param, /*dpaste.param*0.3, dpaste.param*10,*/ new SpaceTimeNonLocalMaximumStrain( 0.0001, 0.0001*dpaste.param[0][0]), dampaste ) ;
-	paste.getFractureCriterion()->setMaterialCharacteristicRadius(0.003) ;
-	box.setBehaviour(&paste);
-
-	F.setSamplingFactor(&box, 2.) ;
-
-	ViscoElasticOnlyAggregateBehaviour dagg ;
-	std::vector<Feature *> feats = PSDGenerator::get2DConcrete(&F, new Viscoelasticity(PURE_ELASTICITY, dagg.param, 0), 500, 0.004, 0.0006, new PSDBolomeA(), CIRCLE, 1., M_PI, 50000, 0.5) ;
-
-	
+	F.setSamplingNumber(96) ;
 	F.setOrder(LINEAR_TIME_LINEAR) ;
-	F.setDeltaTime(timestep) ;
-	F.setMaxIterationsPerStep(10) ;
-	F.setMinDeltaTime(atof(argv[1])) ;
+	F.setMaxIterationsPerStep( 1024 ) ;
+	double totaltime = 7;//atof(argv[1]) ;
+	F.setDeltaTime(totaltime/100.) ;
+	F.setMinDeltaTime((totaltime/100.)*1e-9) ;
+	size_t seed = 1;//atof(argv[3]) ;
+	ShortTermViscoElasticOnlyPasteBehaviour pastenodamage(16e9,0.3,5.) ;
+	ShortTermViscoElasticOnlyPasteBehaviour virtualpastenodamage(16e9,0.3,2.5) ;
 
-	F.addBoundaryCondition(new BoundingBoxDefinedBoundaryCondition(SET_ALONG_INDEXED_AXIS, BOTTOM_LEFT_AFTER, 0,0)) ;
-	F.addBoundaryCondition(new BoundingBoxDefinedBoundaryCondition(SET_ALONG_INDEXED_AXIS, BOTTOM_AFTER, 0,1)) ;
-/*	F.addBoundaryCondition(new BoundingBoxDefinedBoundaryCondition(SET_ALONG_INDEXED_AXIS, LEFT_AFTER, 0,2)) ;
-	F.addBoundaryCondition(new BoundingBoxDefinedBoundaryCondition(SET_ALONG_INDEXED_AXIS, BOTTOM_AFTER, 0,3)) ;*/
-	F.step() ;
+	Rectangle * placement= new Rectangle(width,nnotch*1.1, 0., nnotch*0.552) ;
 
-	BoundingBoxDefinedBoundaryCondition * disp = new BoundingBoxDefinedBoundaryCondition(SET_ALONG_INDEXED_AXIS, TOP_AFTER, 0.000008, 1) ;
-	F.addBoundaryCondition(disp) ;
+	Sample left(nullptr, (length-width)*.5, length, (length-width)*.25-length*.5, 0) ;
+	left.isVirtualFeature  = true ;
+	left.setBehaviour(&pastenodamage) ;
+	
+	Sample middle(nullptr, length, depth+nnotch*0.9, 0., length-(depth+nnotch*0.9)*0.5) ;
+	middle.isVirtualFeature = true ;
+	middle.setBehaviour(&pastenodamage) ;
 
-	std::vector<DelaunayTriangle *> trg = F.getElements2D() ;
-	int count = 0 ;
-	for(size_t i = 0 ; i < trg.size() ; i++)
+	Sample right(nullptr, (length-width)*.5, nnotch*2., length*.5-(length-width)*.25, nnotch) ;
+	right.isVirtualFeature  = true ;
+	right.setBehaviour(&pastenodamage) ;
+
+	Rectangle refinement( width*1.1,nnotch*1.5, 0., nnotch*0.6) ;
+	Rectangle refinement2( width*1,0.001, 0., 0.) ;
+	Rectangle refinement3( width*1,0.001, 0., 0.) ;
+	Rectangle refinement4( width*1,0.00075, 0., 0.) ;
+	Rectangle refinement5( width*1,0.0005, 0., 0.) ;
+	Rectangle refinement6( width*1,0.0005, 0., 0.) ;
+//	F.addRefinementZone( &refinement ) ;
+//	F.addRefinementZone( &refinement2 ) ;
+//	F.addRefinementZone( &refinement3 ) ;
+// 	F.addRefinementZone( &refinement4 ) ;
+// 	F.addRefinementZone( &refinement5 ) ;
+// 	F.addRefinementZone( &refinement6 ) ;
+	
+	ShortTermViscoDamagePasteBehaviour paste(16e9,0.3,2.5) ;
+	paste.freeblocks = 0 ;
+//	if(argv[2] == std::string("strain"))
 	{
-		if(trg[i]->getBehaviour()->getFractureCriterion())
-		{
-			if(std::abs(trg[i]->getCenter().x) > 0.038 && std::abs(trg[i]->getCenter().y) < 0.002)
-			{
-				dynamic_cast<Viscoelasticity *>(trg[i]->getBehaviour())->param *= 2. ;
-				count++ ;
-			}
-		}
+		paste.ctype = STRAIN_CRITERION ;
+		paste.up = 0.00035 ;
 	}
-	std::cout << "paste weakened in " << count << " elements" << std::endl ;
-
- 	
-	std::fstream out ;
-	std::string name = "tensile_" ;
-	name.append(argv[1]) ;
-	
-	out.open(name.c_str(), std::ios::out) ;
-
-	double time = timestep ;	
-	Vector stress = F.getAverageField(REAL_STRESS_FIELD,-1,1) ;
-	Vector strain = F.getAverageField(STRAIN_FIELD,-1,1) ;
-	Vector rate = F.getAverageField(STRAIN_RATE_FIELD,-1,1) ;
-	Vector displ = F.getDisplacements() ;
-	out << std::setprecision(16) << 0. << "\t" << displ.max() << "\t" << displ.min() << "\t" <<  stress[0] << "\t" << stress[1] << "\t" << stress[2] << 
-		"\t" << strain[0] << "\t" << strain[1] << "\t" << strain[2] << 
-		"\t" << rate[0] << "\t" << rate[1] << "\t" << rate[2] << std::endl ;
-
-	std::string nametrg = name ;
-	nametrg.append("_trg_0") ;
-	TriangleWriter writer(nametrg, &F, 1) ;
-	writer.getField(STRAIN_FIELD) ;
-	writer.getField(REAL_STRESS_FIELD) ;
-	writer.getField(TWFT_STIFFNESS) ;
-	writer.getField(TWFT_DAMAGE) ;
-	writer.write() ;
-
-	bool goOn = false ;
-	int i = 0 ;
-	
-	while(!goOn)
+/*	if(argv[2] == std::string("stress"))
 	{
-		i++ ;
+		paste.ctype = STRESS_CRITERION ;
+		paste.up = 0.0002 ;
+	}
+	if(argv[2] == std::string("mixed"))
+	{
+		paste.ctype = MIXED_CRITERION ;
+		paste.up = 0.0002 ;
+		paste.stressFraction = 0.714 ;
+	}*/
+	paste.materialRadius = 0.002 ;
+
+//	if(argv[2] == std::string("elastic"))
+//		box.setBehaviour( &virtualpastenodamage ) ;
+//	else
+		box.setBehaviour( &paste ) ;
+
+	top.setBehaviour( new VoidForm() ) ;
+	notch.setBehaviour( new VoidForm() ) ;
+
+	ViscoElasticOnlyAggregateBehaviour agg ;
+	agg.freeblocks = -1 ;
+
+	std::vector<Geometry *> exclusionZones ;
+	exclusionZones.push_back( notch.getPrimitive() ) ;
+
+/*	std::vector<Feature *> aggregates = PSDGenerator::get2DConcrete( &F, &agg, 60, 0.008, 0.0003, new PSDBolomeA(), CIRCLE, 1., M_PI, 100000, 0.8, placement, exclusionZones, seed ) ;
+	for(size_t i = 30 ; i < aggregates.size() ; i++)
+	{
+		F.setSamplingFactor(aggregates[i], 2.5) ;
+	}*/
+
+ 	F.addFeature(&box, &top) ;
+	F.addFeature(&box, &middle) ;
+ 	F.addFeature(&middle, &notch) ;
+	F.setSamplingFactor(&box, .125) ;
+//	F.setSamplingFactor(&top, 10.) ;
+//	F.setSamplingFactor(&notch, 10.) ;
+	F.addFeature(&box, &left) ;
+	F.addFeature(&box, &right) ;
+	F.setSamplingRestriction( SAMPLE_RESTRICT_4 ) ;
+
+// 	F.addBoundaryCondition(new BoundingBoxAndRestrictionDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, LEFT_AFTER, -1e-5, 1e-5, -1e-5, nnotch+1e-5,0, 0 )) ;
+ //	F.addBoundaryCondition(new BoundingBoxAndRestrictionDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, LEFT_AFTER, -1e-5, 1e-5, -1e-5, nnotch+1e-5, 0, 2 )) ;
+ //	F.addBoundaryCondition(new BoundingBoxAndRestrictionDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, LEFT_AFTER, -1e-5, 1e-5, -1e-5, nnotch+1e-5, 0, 4 )) ;
+// 	F.addBoundaryCondition(new BoundingBoxDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, LEFT_AFTER, 0, 6 )) ;
+ //	F.addBoundaryCondition(new BoundingBoxDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, LEFT_AFTER, 0, 8 )) ;
+
+	F.addBoundaryCondition( new BoundingBoxNearestNodeDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, BOTTOM_AFTER, Point(0., 0.), 0, 1 ) ) ;
+ 	F.addBoundaryCondition( new BoundingBoxNearestNodeDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, BOTTOM_AFTER, Point(0., 0.), 0, 3 ) ) ;
+ 	F.addBoundaryCondition( new BoundingBoxNearestNodeDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, BOTTOM_AFTER, Point(0., 0.), 0, 5 ) ) ;
+	F.addBoundaryCondition( new BoundingBoxNearestNodeDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, BOTTOM_AFTER, Point(0., 0.), 0, 0 ) ) ;
+ 	F.addBoundaryCondition( new BoundingBoxNearestNodeDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, BOTTOM_AFTER, Point(0., 0.), 0, 2 ) ) ;
+ 	F.addBoundaryCondition( new BoundingBoxNearestNodeDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, BOTTOM_AFTER, Point(0., 0.), 0, 4 ) ) ;
+// 	F.addBoundaryCondition( new BoundingBoxNearestNodeDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, BOTTOM_AFTER, Point(0., -length*0.5), 0, 7 ) ) ;
+ //	F.addBoundaryCondition( new BoundingBoxNearestNodeDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, BOTTOM_AFTER, Point(0., -length*0.5), 0, 9 ) ) ;
+
+//	BoundingBoxAndRestrictionDefinedBoundaryCondition * disp = new BoundingBoxAndRestrictionDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, TOP_AFTER, -0.1*width, width*0.6, length*0.99, length*1.01, 0., 0 ) ;
+	BoundingBoxNearestNodeDefinedBoundaryCondition * dispxr = new BoundingBoxNearestNodeDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, TOP_AFTER, Point(width*0.5, length), 0., 0) ;
+	BoundingBoxNearestNodeDefinedBoundaryCondition * dispyr = new BoundingBoxNearestNodeDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, TOP_AFTER, Point(width*0.5, length), 0., 1) ;
+	BoundingBoxNearestNodeDefinedBoundaryCondition * dispxl = new BoundingBoxNearestNodeDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, TOP_AFTER, Point(width*(-0.5), length), 0., 0) ;
+	BoundingBoxNearestNodeDefinedBoundaryCondition * dispyl = new BoundingBoxNearestNodeDefinedBoundaryCondition( SET_ALONG_INDEXED_AXIS, TOP_AFTER, Point(width*(-0.5), length), 0., 1) ;
+	F.addBoundaryCondition(dispxr) ;
+	F.addBoundaryCondition(dispyr) ;
+	F.addBoundaryCondition(dispxl) ;
+	F.addBoundaryCondition(dispyl) ;
+
+	
+	F.step() ;
+	F.getAssembly()->setEpsilon(1e-8) ;
+	Vector x = F.getAverageField(STRAIN_FIELD) ;
+ 	Vector y = F.getAverageField(REAL_STRESS_FIELD) ;
+//	std::cout << 0. << "\t" << x[0] << "\t" << y[0] << std::endl ;
+
+	size_t i = 0 ;
+	size_t j = 0 ;
+
+	std::fstream out ;
+	std::string tata = "alain_" ;
+/*	tata.append(argv[2]) ;
+	tata.append("_") ;
+	tata.append(argv[1]) ;
+	tata.append("_") ;
+	tata.append(argv[3]) ;*/
+	tata.append(".txt") ;
+	out.open(tata.c_str(), std::ios::out) ;
+
+	double speed = 0.0005/totaltime ;
+	double totaldisp = 0.;
+	bool goOn = true ;
+	std::vector<DelaunayTriangle *> trg = F.getElements2D() ;
+	std::cout << y[0] << std::endl ;
+
+	while(speed*F.getCurrentTime() <= 0.0005 && y[0] > -1.)
+	{
+		std::cout << y[0] << std::endl ;
+		if(goOn)
+		{
+			i++ ;
+			totaldisp = speed * F.getCurrentTime() ;
+			dispxr->setData( totaldisp*0.5 ) ;
+			dispyr->setData( totaldisp*0.25/(-1.8666) ) ;
+			dispxl->setData( totaldisp*(-0.5) ) ;
+			dispyl->setData( totaldisp*(0.25)/(-1.8666) ) ;
+			F.setDeltaTime(totaltime/100.) ;
+			F.setMinDeltaTime((totaltime/100.)*1e-9) ;
+			std::cout << totaldisp << std::endl ;
+		}
+
 		goOn = F.step() ;
 
-		stress = F.getAverageField(REAL_STRESS_FIELD,-1,-1) ;
-		strain = F.getAverageField(STRAIN_FIELD,-1,-1) ;
-		rate = F.getAverageField(STRAIN_RATE_FIELD,-1,-1) ;
-		displ = F.getDisplacements(-1, false) ;
-		out << std::setprecision(16) << (F.getElements2D()[0])->getBoundingPoint(0).t << "\t" << displ.max() << "\t" << displ.min() << "\t" << "\t" << stress[0] << "\t" << stress[1] << "\t" << stress[2] << 
-			"\t" << strain[0] << "\t" << strain[1] << "\t" << strain[2] << 
-			"\t" << rate[0] << "\t" << rate[1] << "\t" << rate[2] << std::endl ;
-		
-		std::string nametrg = name ;
-		nametrg.append("_trg_") ;
-		nametrg.append(std::to_string((int) i)) ;
-		TriangleWriter writer(nametrg, &F, -1) ;
-		writer.getField(STRAIN_FIELD) ;
-		writer.getField(REAL_STRESS_FIELD) ;
-		writer.getField(TWFT_STIFFNESS) ;
-		writer.getField(TWFT_DAMAGE) ;
-		writer.write() ;
-		std::cout << nametrg << std::endl ;
+		if(i%10 == 0)
+		{
+			if(goOn)
+			{
+				std::string tati = tata ; tati.append("_") ;
+				tati.append(itoa(i)) ;
+				std::cout << tati << std::endl ;
+				TriangleWriter writer(tati, &F, 1) ;
+	 			writer.getField(STRAIN_FIELD) ;
+		 		writer.getField(REAL_STRESS_FIELD) ;
+				writer.getField(TWFT_DAMAGE) ;
+				writer.getField(TWFT_STIFFNESS) ;
+				writer.write() ;
+				j = 0 ;
+			}
+			if(!goOn)
+			{
+				j++ ;
+				std::string tati = tata ; tati.append("_") ;
+				tati.append(itoa(i)) ; tati.append("_inter_") ;
+				tati.append(itoa(j)) ;
+				std::cout << tati << std::endl ;
+				TriangleWriter writer(tati, &F, -1) ;
+		 		writer.getField(STRAIN_FIELD) ;
+	 			writer.getField(REAL_STRESS_FIELD) ;
+				writer.getField(TWFT_DAMAGE) ;
+				writer.getField(TWFT_STIFFNESS) ;
+				writer.write() ;
+			}
+		}
+
+		if(goOn)
+		{
+	 		x = F.getAverageField(STRAIN_FIELD, -1, 1) ;
+	 		y = F.getAverageField(REAL_STRESS_FIELD, -1, 1) ;
+			out << trg[0]->getBoundingPoint(3).t << "\t" << (F.getCurrentTime()-totaltime/100.)*speed << "\t" << x[0] << "\t" << y[0] << "\t" << F.averageDamage << std::endl ;
+		}
 	}
-
-	stress = F.getAverageField(REAL_STRESS_FIELD,-1,1) ;
-	strain = F.getAverageField(STRAIN_FIELD,-1,1) ;
-	rate = F.getAverageField(STRAIN_RATE_FIELD,-1,1) ;
-	displ = F.getDisplacements(-1, false) ;
-	out << std::setprecision(16) << 1. << "\t" << displ.max() << "\t" << displ.min() << "\t" << "\t" << stress[0] << "\t" << stress[1] << "\t" << stress[2] << 
-			"\t" << strain[0] << "\t" << strain[1] << "\t" << strain[2] << 
-			"\t" << rate[0] << "\t" << rate[1] << "\t" << rate[2] << std::endl ;
-
-	std::string nametrg2 = name ;
-	nametrg2.append("_trg_") ;
-	nametrg2.append(std::to_string((int) i+1)) ;
-	TriangleWriter writer2(nametrg2, &F, 1) ;
-	writer2.getField(STRAIN_FIELD) ;
-	writer2.getField(REAL_STRESS_FIELD) ;
-	writer2.getField(TWFT_STIFFNESS) ;
-	writer2.getField(TWFT_DAMAGE) ;
-	writer2.write() ;
-
-		
+	
+//	F.getAssembly()->print() ;
+	
 	return 0 ;
 }
+
