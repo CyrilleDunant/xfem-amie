1e662864e3c9596d3a26d0afb04e4854c6332a4e
giorla <alain.b.giorla@gmail.com>
2014-08-05 13:38:39 +0000

    missing file added
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@2771 64fb8e13-074e-4207-b257-2d34fb7690c6

---
+++ physics/logarithmic_creep_with_external_parameters.cpp
@@ -0,0 +1,130 @@
+#include "logarithmic_creep_with_external_parameters.h"
+#include "../elements/generalized_spacetime_viscoelastic_element_state.h"
+
+using namespace Amie ;
+
+LogarithmicCreepWithExternalParameters::LogarithmicCreepWithExternalParameters(std::string args, FractureCriterion * c, DamageModel * d, SpaceDimensionality dim, char sep) : LogarithmicCreepWithImposedDeformationAndFracture( Matrix( 3+3*(dim == SPACE_THREE_DIMENSIONAL), 3+3*(dim == SPACE_THREE_DIMENSIONAL)), Vector(), c,d), external(parseDefaultValues(args, sep))
+{
+
+}
+
+LogarithmicCreepWithExternalParameters::LogarithmicCreepWithExternalParameters(std::string args, SpaceDimensionality dim, char sep) : LogarithmicCreepWithImposedDeformationAndFracture( Matrix( 3+3*(dim == SPACE_THREE_DIMENSIONAL), 3+3*(dim == SPACE_THREE_DIMENSIONAL)), Vector()), external(parseDefaultValues(args, sep))
+{
+    noFracture = true ;
+
+}
+
+ElementState * LogarithmicCreepWithExternalParameters::createElementState( IntegrableEntity * e)
+{
+    return new GeneralizedSpaceTimeViscoElasticElementStateWithInternalVariables(e, external) ;
+}
+
+Form * LogarithmicCreepWithExternalParameters::getCopy() const
+{
+    LogarithmicCreepWithExternalParameters * copy ;
+
+    if(noFracture)
+    {
+        copy = new LogarithmicCreepWithExternalParameters( std::string(),  (param.numCols() == 6) ? SPACE_TWO_DIMENSIONAL : SPACE_THREE_DIMENSIONAL)  ;
+    }
+    else
+    {
+        copy = new LogarithmicCreepWithExternalParameters( std::string(),criterion->getCopy(), dfunc->getCopy(),  (param.numCols() == 6) ? SPACE_TWO_DIMENSIONAL : SPACE_THREE_DIMENSIONAL ) ;
+        copy->dfunc->getState(true).resize(dfunc->getState().size());
+        copy->dfunc->getState(true) = dfunc->getState() ;
+        copy->criterion->setMaterialCharacteristicRadius(criterion->getMaterialCharacteristicRadius()) ;
+        copy->dfunc->setDamageDensityTolerance(dfunc->getDamageDensityTolerance());
+        copy->dfunc->setThresholdDamageDensity(dfunc->getThresholdDamageDensity());
+    }
+
+    copy->external = external ;
+    for(size_t i = 0 ; i < relations.size() ; i++)
+        copy->relations.push_back(relations[i]) ;
+
+/*    if(getExtra2dMeshes())
+    {
+        for(size_t i = 0 ; i < getExtra2dMeshes()->size() ; i++)
+            copy->addMesh((*getExtra2dMeshes())[i]);
+    }
+    if(getExtra3dMeshes())
+    {
+        for(size_t i = 0 ; i < getExtra3dMeshes()->size() ; i++)
+            copy->addMesh((*getExtra3dMeshes())[i]);
+    }*/
+
+    return copy ;
+
+}
+
+void LogarithmicCreepWithExternalParameters::step(double timestep, ElementState &s, double maxScore)
+{
+    for(size_t i = 0 ; i < relations.size() ; i++)
+        relations[i]->step( dynamic_cast<GeneralizedSpaceTimeViscoElasticElementStateWithInternalVariables&>(s)) ;
+
+    LogarithmicCreepWithImposedDeformationAndFracture::step(timestep, s, maxScore) ;
+}
+
+void LogarithmicCreepWithExternalParameters::preProcess( double timeStep, ElementState & currentState )
+{
+    GeneralizedSpaceTimeViscoElasticElementStateWithInternalVariables& state = dynamic_cast<GeneralizedSpaceTimeViscoElasticElementStateWithInternalVariables&>(currentState) ;
+    if(state.has("imposed_deformation"))
+        state.set("imposed_deformation", 0.) ;
+
+
+    for(size_t i = 0 ; i < relations.size() ; i++)
+        relations[i]->preProcess( state ) ;
+
+    if(state.has("bulk_modulus"))
+    {
+        double k_inst = state.get("bulk_modulus", external) ;
+        double mu_inst = state.get("shear_modulus", external) ;
+        C = Material::cauchyGreen( k_inst, mu_inst, false, (param.numCols()==6) ? SPACE_TWO_DIMENSIONAL : SPACE_THREE_DIMENSIONAL) ;
+    }
+    else
+    {
+        double E_inst = state.get("young_modulus", external) ;
+        double nu_inst = state.get("poisson_ratio", external) ;
+        C = Material::cauchyGreen( E_inst, nu_inst, true, (param.numCols()==6) ? SPACE_TWO_DIMENSIONAL : SPACE_THREE_DIMENSIONAL) ;
+    }
+    placeMatrixInBlock( C, 0,0, param) ;
+    if(state.has("creep_characteristic_time"))
+    {
+        isPurelyElastic = false ;
+        if(state.has("creep_bulk"))
+        {
+            double k_visc = state.get("creep_bulk", external) ;
+            double mu_visc = state.get("creep_shear", external) ;
+            C = Material::cauchyGreen( k_visc, mu_visc, false, (param.numCols()==6) ? SPACE_TWO_DIMENSIONAL : SPACE_THREE_DIMENSIONAL) ;
+        }
+        else
+        {
+            double E_visc = state.get("creep_modulus", external) ;
+            double nu_visc = state.get("creep_poisson", external) ;
+            E = Material::cauchyGreen( E_visc, nu_visc, true, (param.numCols()==6) ? SPACE_TWO_DIMENSIONAL : SPACE_THREE_DIMENSIONAL) ;
+        }
+        tau = state.get("creep_characteristic_time", external) ;
+        placeMatrixInBlock( C*(-1), 1,0, param) ;
+        placeMatrixInBlock( C*(-1), 0,1, param) ;
+        placeMatrixInBlock( C, 1,1, param) ;
+        placeMatrixInBlock( E, 1,1, eta) ;
+    }
+    else
+    {
+        model = PURE_ELASTICITY ;
+        isPurelyElastic = true ;
+    }
+    if(state.has("imposed_deformation"))
+    {
+        imposed.resize(C.numCols()) ;
+        double a = state.get("imposed_deformation", external) ;
+        for(size_t i = 0 ; i < 2+(imposed.size()==6) ; i++)
+            imposed[i] = a ;
+    }
+    else
+        imposed.resize(0) ;
+
+    LogarithmicCreepWithImposedDeformationAndFracture::preProcess(timeStep, currentState) ;
+
+    currentState.getParent()->behaviourUpdated = true ;
+
+}
---
+++ physics/logarithmic_creep_with_external_parameters.h
@@ -0,0 +1,69 @@
+// C++ Interface: logarithmic_creep
+//
+// Description: Logarithmic visco-elastic behaviour for the Space-Time Finite Element Method
+//
+//
+// Author: Alain Giorla <alain.giorla@epfl.ch>, (C) 2007-2014
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#ifndef __LOGARITHMIC_CREEP_WITH_EXTERNAL_PARAMETERS_H_
+#define __LOGARITHMIC_CREEP_WITH_EXTERNAL_PARAMETERS_H_
+
+#include "logarithmic_creep.h"
+#include "../elements/generalized_spacetime_viscoelastic_element_state.h"
+#include "material_laws/material_laws.h"
+
+namespace Amie
+{
+
+/* This is a generic mechanical behaviour. It is composed by a spring and a non-linear (logarithmic) dashpot placed in parallels with an imposed deformation.
+ *
+ * The behaviour relies on two components: a list of material parameters (mapped with their default values) and a list of material laws. The parameters and the laws must be set prior to any calculation
+ *
+ * The list of material parameters must include:
+ *      - "young_modulus" and "poisson_ratio" for the material to be valid (or alternatively "bulk_modulus" and "shear_modulus")
+ *      - "creep_modulus", "creep_poisson" and "creep_characteristic_time" for the creep law to activate (or "creep_bulk", "creep_shear" and "creep_characteristic_time") ; without the material is considered as purely elastic
+ *      - "imposed_deformation" for the imposed strain to activate ; otherwise the material has an imposed strain of 0.
+ *
+ * During the pre- and post-processing, the material laws are applied. They are called in order of appearance in the vector of material laws.
+ *
+ * The local values of the parameters are stored in the element state (explicitely described as a GeneralizedSpaceTimeViscoElasticElementStateWithInternalVariables ) and not in the behaviour. The behaviour contains the default values for the parameters in case they are not found in the element state.
+ */
+struct LogarithmicCreepWithExternalParameters : public LogarithmicCreepWithImposedDeformationAndFracture
+{
+protected:
+    std::map<std::string, double> external ;
+    std::vector< ExternalMaterialLaw * > relations ;
+
+public:
+    // the argument string contains the list of external variables and their default values (see the function parseDefaultValues() in external_material_laws.h for more details)
+    LogarithmicCreepWithExternalParameters(std::string args, SpaceDimensionality dim = SPACE_TWO_DIMENSIONAL, char sep = ',') ;
+    LogarithmicCreepWithExternalParameters(std::string args, FractureCriterion * c, DamageModel * d, SpaceDimensionality dim = SPACE_TWO_DIMENSIONAL, char sep = ',') ;
+    virtual ~LogarithmicCreepWithExternalParameters() { if(dfunc) {delete dfunc ; } if(criterion) {delete criterion ;} }
+
+    virtual ElementState * createElementState( IntegrableEntity * e) ;
+
+    virtual Form * getCopy() const ;
+
+    virtual void step(double timestep, ElementState &s, double maxScore) ;
+
+    virtual void preProcess( double timeStep, ElementState & currentState ) ;
+
+    void setMaterialParameters( std::map<std::string, double> & e) { external = e ; }
+    void addMaterialParameter( std::string name, double defaultValue = 0) { external[name] = defaultValue ; }
+
+    void addMaterialLaw( ExternalMaterialLaw * law) { relations.push_back(law) ; }
+    void addMaterialLaws( std::vector<ExternalMaterialLaw *> laws) { for(size_t i = 0 ; i < laws.size() ; i++) {relations.push_back(laws[i]);} }
+    void addMaterialLaws( ExternalMaterialLaw* laws[]) { int length = sizeof(laws)/sizeof(ExternalMaterialLaw*) ; for(size_t i = 0 ; i < length ; i++) {relations.push_back(laws[i]);} }
+
+
+} ;
+
+
+} ;
+
+
+#endif
