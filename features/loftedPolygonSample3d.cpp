0dda01beeb852f1263177e65284039c6083d0081
cdunant <cdunant@unknown.com>
2015-02-13 15:22:54 +0000

    * add files so it compiles
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@2893 64fb8e13-074e-4207-b257-2d34fb7690c6

---
+++ features/loftedPolygonSample3d.cpp
@@ -0,0 +1,55 @@
+//
+// C++ Implementation: sample
+//
+// Description: 
+//
+//
+// Author: Cyrille Dunant <cyrille.dunant@gmail.com>, (C) 2006-2011
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#include "loftedPolygonSample3d.h"
+#include "../physics/void_form.h"
+
+using namespace Amie ;
+
+LoftedPolygonalSample3D::LoftedPolygonalSample3D(Feature *father, const std::valarray<Point *> & points, const std::vector<Point> & interpolationPoints, const std::vector<Point> & tangents) : LoftedPolygonPrism(points,  interpolationPoints,  tangents), Feature(father)
+{
+	this->isEnrichmentFeature = false ;
+	this->behaviour = nullptr ;
+}
+
+
+std::vector<DelaunayTetrahedron *> LoftedPolygonalSample3D::getElements3D( FeatureTree * dt) 
+{
+	std::vector<DelaunayTetrahedron *>  ret ;
+	std::vector<DelaunayTetrahedron *>  temp = dt->get3DMesh()->getConflictingElements(getPrimitive()) ;
+	
+	for(size_t i = 0 ; i < temp.size() ; i++)
+	{
+		bool inChild = false ;
+		for(size_t j = 0 ;  j< this->getChildren().size() ;  j++)
+		{
+			if(this->getChild(j)->in(temp[i]->getCenter()))
+			{
+				inChild = true ; 
+				break ;
+			}
+		}
+		if(this->LoftedPolygonalSample3D::in(temp[i]->getCenter()) && inChild == false)
+			ret.push_back(temp[i]) ;
+	}
+	
+	return ret ;
+}
+
+bool LoftedPolygonalSample3D::interacts(Feature * f, double d) const
+{
+	for(PointSet::const_iterator i =this->begin() ; i < this->end() ; i++)
+		if(f->inBoundary(*(*i), d))
+			return true ;
+	return false ;
+}
+
---
+++ features/loftedPolygonSample3d.h
@@ -0,0 +1,65 @@
+//
+// C++ Interface: sample
+//
+// Description:
+//
+//
+// Author: Cyrille Dunant <cyrille.dunant@gmail.com>, (C) 2006-2011
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef __LOFT_POLY_SAMPLE_3D_H__
+#define __LOFT_POLY_SAMPLE_3D_H__
+
+#include "features.h"
+
+namespace Amie
+{
+
+class LoftedPolygonalSample3D :  public LoftedPolygonPrism,  public Feature
+{
+public:
+
+    LoftedPolygonalSample3D(Amie::Feature* father, const std::valarray<Point *> & points, const std::vector<Point> & interpolationPoints, const std::vector<Point> & tangents) ;
+
+    virtual bool interacts(Feature * f, double d) const ;
+
+    /** \brief return empty list*/
+    virtual std::vector<Geometry *> getRefinementZones(size_t) const {
+        return std::vector<Geometry *>() ;
+    }
+
+    /** \brief return all triangles in mesh with at least a vertex in this Feature*/
+    virtual std::vector<DelaunayTriangle *> getElements2D( FeatureTree * dt)  {
+        return std::vector<DelaunayTriangle *>(0) ;
+    }
+
+    /** \brief return empty vector*/
+    virtual std::vector<DelaunayTetrahedron *> getElements3D( FeatureTree * dt) ;
+
+    virtual void print() const
+    {
+        std::cout << "I am a polygonal sample" << std::endl ;
+    }
+
+    /** \brief return false */
+    virtual bool isVoid( const Point &p) const {
+        return false;
+    }
+
+public:
+
+    GEO_DERIVED_OBJECT(LoftedPolygonPrism) ;
+
+    virtual void sample(size_t n)
+    {
+        this->sampleSurface(n) ;
+    }
+
+} ;
+
+
+} ;
+
+#endif
