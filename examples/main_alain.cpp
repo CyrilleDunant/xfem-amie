5c140bc96f7e7dbadd0f6739c00e09b794d014d1
alain.b.giorla@gmail.com <alain.b.giorla@gmail.com>
2015-03-12 17:24:26 +0000

    test case for debug
    git-svn-id: svn+ssh://c4science.ch/source/amie/trunk@2912 64fb8e13-074e-4207-b257-2d34fb7690c6

--- examples/main_alain.cpp
+++ examples/main_alain.cpp
@@ -17,7 +17,6 @@
 #include "../physics/parallel_behaviour.h"
 //#include "../physics/generalized_spacetime_viscoelasticity.h"
 #include "../physics/fracturecriteria/mohrcoulomb.h"
-#include "../physics/fracturecriteria/spacetimemultilinearsofteningfracturecriterion.h"
 #include "../physics/fracturecriteria/ruptureenergy.h"
 #include "../physics/weibull_distributed_stiffness.h"
 #include "../features/pore.h"
@@ -43,6 +42,7 @@
 #include "../utilities/itoa.h"
 #include "../physics/damagemodels/spacetimefiberbasedisotropiclineardamage.h"
 #include "../physics/fracturecriteria/maxstrain.h"
+#include "../physics/fracturecriteria/spacetimemultilinearsofteningfracturecriterion.h"
 
 #include <fstream>
 #include <omp.h>
@@ -57,81 +57,46 @@ using namespace Amie ;
 
 int main(int argc, char *argv[])
 {
-//    omp_set_num_threads(1) ;
+	Sample box(nullptr, 0.01,0.01,0.,0.) ;
 
-    Sample box(nullptr, 0.08,0.08,0.,0.) ;
-
-
-    FeatureTree F(&box) ;
-    F.setSamplingNumber(32) ;
-    F.setOrder(LINEAR_TIME_LINEAR) ;
-    double time_step = 0.01 ;
-    F.setDeltaTime(time_step) ;
-    F.setMinDeltaTime(1e-9) ;
-    F.setSamplingRestriction( SAMPLE_RESTRICT_4 ) ;
-
-    std::vector<Point> p ;
-    p.push_back( Point( 1e6/1e9, 1e6 ) ) ;
-    p.push_back( Point( 2e6/1e9, 0e6 ) ) ;
-    std::vector<Point> c ;
-    AsymmetricSpaceTimeNonLocalMultiLinearSofteningFractureCriterion crit( p, c, 1e9/*, 0.1*/ ) ;
-    
-    LogarithmicCreepWithExternalParameters paste("young_modulus = 12e9, poisson_ratio = 0.2, creep_modulus = 30e9, creep_characteristic_time = 1, creep_poisson = 0.2", &crit, new SpaceTimeFiberBasedIsotropicLinearDamage(0.9999, 1e-9, 0.99)) ;
-    box.setBehaviour( &paste );
-
-    LogarithmicCreepWithExternalParameters aggregates("young_modulus = 60e9, poisson_ratio = 0.2") ;
-
-    PSDGenerator::get2DConcrete(&F, &aggregates, 500, 0.01, 0.00001, new PSDBolomeA(), CIRCLE, 1., M_PI, 1000000, 0.9) ;
-
-    F.step() ;
-
-    F.addBoundaryCondition( new BoundingBoxDefinedBoundaryCondition( FIX_ALONG_ETA, BOTTOM_AFTER) ) ;
-    F.addBoundaryCondition( new BoundingBoxDefinedBoundaryCondition( FIX_ALONG_XI, LEFT_AFTER ) ) ;
-    BoundingBoxDefinedBoundaryCondition * load = new BoundingBoxDefinedBoundaryCondition( SET_STRESS_ETA, TOP_AFTER, 1e6 ) ;
-    F.addBoundaryCondition(load) ;
-
-    F.setMaxIterationsPerStep(256) ;
-
-    std::string toto = "test_damage";
-    std::fstream out ;
-    out.open(toto.c_str(), std::ios::out) ;
-    int i = 0 ;
-
-    while(F.getCurrentTime() < 100)
-    {
-	i++ ;
-	time_step *= 1.1 ;
+	FeatureTree F(&box) ;
+	F.setSamplingNumber(16) ;
+	F.setOrder(LINEAR_TIME_LINEAR) ;
+	double time_step = 1. ;
 	F.setDeltaTime(time_step) ;
-//	load->setData(0.0001*F.getCurrentTime()) ;
-	while(!F.step()) { 
-		i++ ;
-		std::cout << std::endl ;
-		std::string test = "test_" ;
-		test.append(itoa(i)) ;
-		TriangleWriter writer(test.c_str(), &F, 1.) ;
-		writer.getField(STRAIN_FIELD) ;
-		writer.getField(PRINCIPAL_REAL_STRESS_FIELD) ;
-		writer.getField(SCALAR_DAMAGE_FIELD) ;
-		writer.getField(TWFT_STIFFNESS) ;
-		writer.write() ;
-	} ;
-	std::cout << "\n" << F.getAverageField( STRAIN_FIELD, -1, 1.)[1] << "\t" << F.getAverageField( REAL_STRESS_FIELD, -1, 1.)[1] << std::endl; 
-	out << F.getCurrentTime() << "\t"<< F.getAverageField( STRAIN_FIELD, -1, 1.)[1] << "\t" << F.getAverageField( REAL_STRESS_FIELD, -1, 1.)[1] << std::endl; 
 
-	std::string test = "test_" ;
-	test.append(itoa(i)) ;
-	TriangleWriter writer(test.c_str(), &F, 1.) ;
-	writer.getField(STRAIN_FIELD) ;
-	writer.getField(PRINCIPAL_REAL_STRESS_FIELD) ;
-	writer.getField(SCALAR_DAMAGE_FIELD) ;
-	writer.getField(TWFT_STIFFNESS) ;
-	writer.write() ;
+	F.addBoundaryCondition( new BoundingBoxDefinedBoundaryCondition( FIX_ALONG_XI, LEFT_AFTER) ) ;
+	F.addBoundaryCondition( new BoundingBoxDefinedBoundaryCondition( FIX_ALONG_ETA, BOTTOM_AFTER) ) ;
+	BoundingBoxDefinedBoundaryCondition * disp = new BoundingBoxDefinedBoundaryCondition( SET_ALONG_ETA, TOP_AFTER, 0) ;
+	F.addBoundaryCondition(disp) ;
 
-    }
+	Point p0(0.0001,1.5e6) ;
+	Point p1(0.0003,0.) ;
+	std::vector<Point> tensionCurve ;
+	std::vector<Point> compressionCurve ;
+	tensionCurve.push_back(p0) ;
+	tensionCurve.push_back(p1) ;
+	AsymmetricSpaceTimeNonLocalMultiLinearSofteningFractureCriterion * crit =  new AsymmetricSpaceTimeNonLocalMultiLinearSofteningFractureCriterion( tensionCurve, compressionCurve, 15e9) ; 
+	crit->setMaterialCharacteristicRadius(0.001) ;
+
+	LogarithmicCreepWithExternalParameters paste("young_modulus = 15e9, poisson_ratio = 0.3, creep_modulus = 30e9, creep_poisson = 0.3, creep_characteristic_time = 1.", crit, new SpaceTimeFiberBasedIsotropicLinearDamage(0.1,1e-9, 1.) ) ;
+	box.setBehaviour(&paste) ;
+
+	Inclusion * notch = new Inclusion( 0.001, -0.005, 0.) ;
+	notch->setBehaviour(new VoidForm()) ;
+	F.addFeature(&box, notch) ;
 
 
+	while(F.getCurrentTime() < 110)
+	{
+		disp->setData( 0.01*0.001*F.getCurrentTime()/1000) ;
+		time_step += 1. ;
+		F.setDeltaTime(time_step) ;
+		F.step() ;
+		std::cout << F.getCurrentTime() << "\t" << F.getAverageField( STRAIN_FIELD, -1, 1.)[1] << "\t" << F.getAverageField(REAL_STRESS_FIELD, -1, 1)[1] << std::endl ;
+	}
 
-    return 0 ;
 
+	return 0 ;
 }
 
